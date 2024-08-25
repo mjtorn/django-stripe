c20b382a5be241335634e51dce8e72aacc0ac49e
Purnendu Kar <purnendu.kar8@gmail.com>
2024-08-25 14:06:49 +0530

    refactor: reformat

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,18 +5,17 @@ from django_stripe import get_version
 
 
 class DjangoStripeVersionTests(TestCase):
-
     def test_patch_number_is_str(self):
-        result = get_version((0, 1, 'pre'))
+        result = get_version((0, 1, "pre"))
 
-        self.assertEqual(result, '0.1_pre')
+        self.assertEqual(result, "0.1_pre")
 
     def test_patch_number_is_int(self):
         result = get_version((0, 1, 0))
 
-        self.assertEqual(result, '0.1.0')
+        self.assertEqual(result, "0.1.0")
 
     def test_no_patch_number(self):
         result = get_version((0, 1))
 
-        self.assertEqual(result, '0.1')
+        self.assertEqual(result, "0.1")
