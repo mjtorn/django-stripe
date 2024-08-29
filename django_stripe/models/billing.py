66144dbfe1a14503fb5b98dcace324267c872824
Purnendu Kar <purnendu.kar8@gmail.com>
2024-08-30 02:41:11 +0530

    refactor: StripeSubscription model and action

--- django_stripe/models/billing.py
+++ django_stripe/models/billing.py
@@ -1,6 +1,14 @@
+# Third Party Stuff
+from django.db import models
+
 # Django Stripe Stuff
 from django_stripe.models.base.billing import AbstactStripeSubscription
 
 
 class StripeSubscription(AbstactStripeSubscription):
-    pass
+    customer = models.ForeignKey(
+        "django_stripe.StripeCustomer",
+        to_field="stripe_id",
+        on_delete=models.CASCADE,
+        null=True,
+    )
