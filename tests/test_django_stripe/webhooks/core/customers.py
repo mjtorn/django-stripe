0d68989dfa2726d53290d5210472f47b5753dde6
Purnendu Kar <purnendu.kar8@gmail.com>
2024-09-08 23:53:09 +0530

    testcase: customer.created webhook

--- tests/test_django_stripe/webhooks/core/customers.py
+++ tests/test_django_stripe/webhooks/core/customers.py
@@ -1,4 +1,5 @@
 # Third Party Stuff
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -106,3 +107,83 @@ class CustomerUpdatedWebhookTestCase(TestCase):
             updated_customer.description,
             self.event_data["data"]["object"]["description"],
         )  # Updated description
+
+
+class CustomerCreatedWebhookTestCase(TestCase):
+    def setUp(self):
+        # Setup the API client
+        self.client = APIClient()
+
+        User.objects.create(email="purnendu.kar8@gmail.com")
+
+        # Mock event data from Stripe for testing (customer.updated)
+        self.event_data = {
+            "id": "evt_1PwPfEIO5cnPOFxQkKhmOfj9",
+            "object": "event",
+            "api_version": "2024-06-20",
+            "created": 1725719460,
+            "data": {
+                "object": {
+                    "id": "cus_QjC48SZoGC5HV5",
+                    "object": "customer",
+                    "address": None,
+                    "balance": 0,
+                    "created": 1724604770,
+                    "currency": "usd",
+                    "default_source": "card_1PrjirIO5cnPOFxQX13GITTH",
+                    "delinquent": False,
+                    "description": "just desc",  # Updated description
+                    "discount": None,
+                    "email": "purnendu.kar8@gmail.com",
+                    "invoice_prefix": "0A66AED6",
+                    "invoice_settings": {
+                        "custom_fields": None,
+                        "default_payment_method": None,
+                        "footer": None,
+                        "rendering_options": None,
+                    },
+                    "livemode": False,
+                    "metadata": {},
+                    "name": "Purnendu Kar",
+                    "next_invoice_sequence": 2,
+                    "phone": "+918334035754",
+                    "preferred_locales": [],
+                    "shipping": {
+                        "address": {
+                            "city": "",
+                            "country": "",
+                            "line1": "",
+                            "line2": "",
+                            "postal_code": "",
+                            "state": "",
+                        },
+                        "name": "Purnendu Kar",
+                        "phone": "+918334035754",
+                    },
+                    "tax_exempt": "none",
+                    "test_clock": None,
+                },
+            },
+            "livemode": False,
+            "pending_webhooks": 1,
+            "request": {
+                "id": "req_IskUPKAZ7ilcLq",
+                "idempotency_key": "3a8024d0-6a1d-4cca-a891-03ce9ceab2df",
+            },
+            "type": "customer.created",
+        }
+
+    def test_customer_created_webhook(self):
+        # Simulate the webhook POST request for customer.updated
+        response = self.client.post(
+            reverse("stripe-webhook-list"), self.event_data, format="json"
+        )
+
+        # Check that the response is successful
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the customer in the database has been updated
+        updated_customer = StripeCustomer.objects.filter(
+            stripe_id=self.event_data["data"]["object"]["id"]
+        ).first()
+        self.assertIsNotNone(updated_customer)  # Updated description
