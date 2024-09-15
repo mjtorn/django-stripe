6036c99d229e1391b65d786b594a38420239e075
Purnendu Kar <purnendu.kar8@gmail.com>
2024-09-16 00:06:41 +0530

    chore: test pypi

--- django_stripe/__init__.py
+++ django_stripe/__init__.py
@@ -1,21 +1,2 @@
 __author__ = "Purnendu Kar"
-# -*- coding: utf-8 -*-
-VERSION = (0, 1, 0, "DEV")
-
-
-def get_version(version):
-    """Dynamically calculate the version based on VERSION tuple."""
-    if len(version) > 2 and version[2] is not None:
-        if len(version) == 4:
-            str_version = "%s.%s.%s.%s" % version
-        elif isinstance(version[2], int):
-            str_version = "%s.%s.%s" % version[:3]
-        else:
-            str_version = "%s.%s_%s" % version[:3]
-    else:
-        str_version = "%s.%s" % version[:2]
-
-    return str_version
-
-
-__version__ = get_version(VERSION)
+__version__ = "0.1.0"
