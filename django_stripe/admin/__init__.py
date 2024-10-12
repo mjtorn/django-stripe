414c1ade83952f10e39f968de3339f93b0646294
Purnendu Kar <purnendu.kar8@gmail.com>
2024-10-13 01:17:38 +0530

    feat: stripe product admin

--- django_stripe/admin/__init__.py
+++ django_stripe/admin/__init__.py
@@ -1,3 +1,4 @@
-from .core import StripeCustomerAdmin
+from django_stripe.admin.core.customers import StripeCustomerAdmin
+from django_stripe.admin.products.products import StripeProductAdmin
 
-__all__ = ("StripeCustomerAdmin",)
+__all__ = ("StripeCustomerAdmin", "StripeProductAdmin")
---
+++ django_stripe/admin/products/products.py
@@ -0,0 +1,20 @@
+# Third Party Stuff
+from django.contrib import admin
+
+# Django Stripe Stuff
+from django_stripe.admin.abstracts import AbstractStripeModelAdmin
+from django_stripe.models import StripeProduct, StripePrice
+from django_stripe.actions import StripeProductAction
+
+
+class StripeSubscriptionInlineAdmin(admin.StackedInline):
+    model = StripePrice
+    extra = 0
+
+
+@admin.register(StripeProduct)
+class StripeProductAdmin(AbstractStripeModelAdmin):
+    list_display = ("stripe_id", "name", "description")
+    search_fields = ("stripe_id", "name", "description")
+    inlines = [StripeSubscriptionInlineAdmin]
+    stripe_model_action = StripeProductAction()
